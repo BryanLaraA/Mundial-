83e0afb6d38ae305ea0cbf481a422934d916f8d6
isgutierrez <isgutierrez@gmail.com>
2026-06-19 13:56:48 -0600

    se agregan los atributos a la clase partidos
    se le agregan los atributos, metodos de acceso y metodo constructor a la clase partidos

--- src/partidos/Partido.java
+++ src/partidos/Partido.java
@@ -4,10 +4,84 @@
  */
 package partidos;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  *
  * @author Isaac
  */
 public class Partido {
+    private String Equipolocal;
+    private String EquipoVisitante;
+    private LocalDate fechaPartido;
+    private LocalTime horaPartido;
+    private String ubicacion; 
+    private int marcadorz;
+    private int marcadorLocal;
+    private int marcadorVisitante;
+
+    public String getEquipolocal() {
+        return Equipolocal;
+    }
+
+    public String getEquipoVisitante() {
+        return EquipoVisitante;
+    }
+
+    public LocalDate getFechaPartido() {
+        return fechaPartido;
+    }
+
+    public LocalTime getHoraPartido() {
+        return horaPartido;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public int getMarcadorz() {
+        return marcadorz;
+    }
+
+    public int getMarcadorLocal() {
+        return marcadorLocal;
+    }
+
+    public int getMarcadorVisitante() {
+        return marcadorVisitante;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public void setMarcadorz(int marcadorz) {
+        this.marcadorz = marcadorz;
+    }
+
+    public void setMarcadorLocal(int marcadorLocal) {
+        this.marcadorLocal = marcadorLocal;
+    }
+
+    public void setMarcadorVisitante(int marcadorVisitante) {
+        this.marcadorVisitante = marcadorVisitante;
+    }
+
+    public Partido(String Equipolocal, String EquipoVisitante, LocalDate fechaPartido, LocalTime horaPartido, String ubicacion, int marcadorz, int marcadorLocal, int marcadorVisitante) {
+        this.Equipolocal = Equipolocal;
+        this.EquipoVisitante = EquipoVisitante;
+        this.fechaPartido = fechaPartido;
+        this.horaPartido = horaPartido;
+        this.ubicacion = ubicacion;
+        this.marcadorz = marcadorz;
+        this.marcadorLocal = marcadorLocal;
+        this.marcadorVisitante = marcadorVisitante;
+    }
+
+
+    
+    
     
 }
