0c306b9b70dbae3cc0b9872329b4d411285a3a9c
FlyStudent <anporrasnu@est.utn.ac.cr>
2026-06-19 16:56:59 -0600

    Jugador con mayuscula lenox babosillo

--- src/jugadores/jugador.java
+++ src/jugadores/jugador.java
@@ -2,17 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package jugadores;
-
+package jugadores; 
 import java.time.LocalDate;
 import persona.Persona;
-
 /**
  *
  * @author Student
  */
 public class Jugador extends Persona {
-
     protected int numero;
     protected String posicion;
 
