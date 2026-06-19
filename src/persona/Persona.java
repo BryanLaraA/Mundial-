b2fd6d3f65ab757a5edf12a196a0b624c4cdd3c4
Bryan <bryanlaraarguello@gmail.com>
2026-06-19 14:42:33 -0600

    Creacion de la clase persona
    Creacion de los atributos generales nombre,nacionalidad,fechadenacimiento

---
+++ src/persona/Persona.java
@@ -0,0 +1,52 @@
+
+package persona;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Persona {
+    protected String nombre;
+    protected String nacionalidad;
+    protected LocalDate fechadenacimiento;
+    protected int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public LocalDate getFechadenacimiento() {
+        return fechadenacimiento;
+    }
+    
+     public int getEdad(){
+        return Period.between(fechadenacimiento, LocalDate.now()).getYears();
+    }
+    
+
+    public Persona(String nombre, String nacionalidad, LocalDate fechanacimiento, int edad) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.fechadenacimiento = fechanacimiento;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre 
+                + ", nacionalidad=" + nacionalidad 
+                + ", fecha=" + fechadenacimiento + ", edad=" 
+                + edad + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
+            
+}
