f523a6d2fc156ba41fd6b511f8a500defb0976e4
FlyStudent <anporrasnu@est.utn.ac.cr>
2026-06-19 16:01:26 -0600

    Clase Seleccion
    Se crea la clase Selección con sus atributos, getters, setters, agragaciones, metodo de busqueda de nulos, metodo de eliminacion de jugadores y constructor

--- src/selecciones/Seleccion.java
+++ src/selecciones/Seleccion.java
@@ -1,4 +1,60 @@
 package selecciones;
+import jugadores.jugador;
+import entrenadores.Entrenador;
 public class Seleccion {
+    private String nombre;
+    private Grupos grupo;
+    private Entrenador entrenador;
+    private jugador jugadores[];
+    public String getNombre() {
+        return nombre;
+    }
+    public Grupos getGrupo() {
+        return grupo;
+    }
+    public Entrenador getEntrenador() {
+        return entrenador;
+    }
+    public jugador[] getJugadores() {
+        return jugadores;
+    }
+    public void setGrupo(Grupos grupo) {
+        this.grupo = grupo;
+    }
+    public void setEntrenador(Entrenador entrenador) {
+        this.entrenador = entrenador;
+    }
+
+    public Seleccion(String nombre, Grupos grupo, Entrenador entrenador, jugador[] jugadores) {
+        this.nombre = nombre;
+        this.grupo = grupo;
+        this.entrenador = entrenador;
+        this.jugadores = jugadores;
+    }
     
+    public void deleteJugadores(jugador jugador){
+        while(true){
+            
+        }
+    }
+    public void addJugadores(jugador jugador){
+        this.jugadores[buscarVacio()]=jugador;
+    }
+    public int buscarVacio(){
+        int i=0;
+        while(true){
+            if(jugadores[i]==null){
+                return i;
+            }
+            i++;
+        }      
+    }
+    public Seleccion(String nombre, Grupos grupo, Entrenador entrenador, jugador jugado) {
+        this.nombre = nombre;
+        this.grupo = grupo;
+    }
+    @Override
+    public String toString() {
+        return "Seleccion: " + "\nnombre: " + nombre + "\ngrupo=" + grupo + "\nentrenador=" + entrenador + "\njugadores=" + jugadores;
+    }
 }
