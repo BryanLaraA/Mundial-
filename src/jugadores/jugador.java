ea9fc0a3b55cc3bc39d06b88d655ea055b49b18b
lennox1 <leherreranu@est.utn.ac.cr>
2026-06-19 14:52:32 -0600

    Se agregaron los atributos del class jugadores

--- src/jugadores/jugador.java
+++ src/jugadores/jugador.java
@@ -8,7 +8,69 @@ package jugadores;
  *
  * @author Student
  */
+
 public class jugador {
-    
+    private String nombre;
+    private int edad;
+    private String posicion;
+    private int numeroCamiseta;
+    private int goles;
+
+    public jugador(String nombre, int edad, String posicion, int numeroCamiseta, int goles) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.posicion = posicion;
+        this.numeroCamiseta = numeroCamiseta;
+        this.goles = goles;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public int getNumeroCamiseta() {
+        return numeroCamiseta;
+    }
+
+    public int getGoles() {
+        return goles;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public void setNumeroCamiseta(int numeroCamiseta) {
+        this.numeroCamiseta = numeroCamiseta;
+    }
+
+    public void setGoles(int goles) {
+        this.goles = goles;
+    }
+
+    public void anotarGol() {
+        this.goles++;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador: " + "nombre='" + nombre +", edad=" + edad +", posicion='" + posicion +", numeroCamiseta=" + numeroCamiseta +", goles=" + goles ;
+    }
+
 }
-//jj
