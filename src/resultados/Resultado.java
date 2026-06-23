1770be8869b5b9bd5b78414d6348dbf164a8745b
lennox <leherreranu@est.utn.ac.cr>
2026-06-23 16:53:49 -0600

    resultados

--- src/resultados/Resultado.java
+++ src/resultados/Resultado.java
@@ -9,5 +9,37 @@ package resultados;
  * @author Student
  */
 public class Resultado {
-    protected int goles;
+
+    private int gol1;
+    private int gol2;
+    private String Equi1;
+    private String Equi2;
+
+   public void partido(){
+    if (gol1 == gol2)
+    {
+        System.out.println("empate");
+    }else{
+    System.out.println("ganador");
+    }
+    int diferente = Math.abs(gol1 - gol2);
+    System.out.println ("diferencia entre goles: "+ diferente);
+    
+    if (gol1 > 0 && gol2 >0){
+    System.out.println("ambos equipos anotaron");
+    }else{
+    System.out.println("ninguno anoto");
+    }
+    
+    System.out.println ("goles "+ Equi1 + ": "+ gol1);
+    System.out.println ("goles"+ Equi2 + ": "+ gol2);
+    
+    if (gol1 > gol2){
+    System.out.println("Ganador: "+ Equi1);
+    }else if (gol2 > gol1){
+    System.out.println("Ganador: "+ Equi2);
+   }else{
+    System.out.println("No hay ganador");
+    }
+  }
 }
