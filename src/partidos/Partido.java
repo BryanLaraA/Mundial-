940bf54442fd6affa668f37fbfb1a60cc77f185c
isaac <isaacpr11371@gmail.com>
2026-06-23 16:09:11 -0600

    Se agregan los metodos agregar, eliminar y buscar
    Se agregan los metodos de agregar, buscar y eliminar por nombre a la clase, para poder agregar arbitros

--- src/partidos/Partido.java
+++ src/partidos/Partido.java
@@ -4,15 +4,17 @@
  */
 package partidos;
 
+import arbitros.Arbitro;
 import java.time.LocalDate;
 /**
  *
  * @author Isaac
  */
-public class Partido {
+public class Partido extends Arbitro{
     protected String nombre; 
     protected LocalDate año;
     protected String pais; 
+    protected String[] arbitro = new String[20];
 
     public String getNombre() {
         return nombre;
@@ -38,16 +40,57 @@ public class Partido {
         this.pais = pais;
     }
 
-    public Partido(String nombre, LocalDate año, String pais) {
+    public Partido( LocalDate año, String pais, String categoria, String nombre, String nacionalidad, LocalDate fechanacimiento) {
+        super(categoria, nombre, nacionalidad, fechanacimiento);
         this.nombre = nombre;
         this.año = año;
         this.pais = pais;
     }
 
-    
+   public boolean agregarArbitro(String newArbitro) {
+    for (int i = 0; i < arbitro.length; i++) {
+        if (arbitro[i].equals(newArbitro)){
+            return false; 
+        } if (arbitro[i] == null) {
+            arbitro[i] = newArbitro;
+            System.out.println("Arbitro agregado");
+            }
+        }
+        return true;
     }
+     public void eliminarArbitro(String nombre){
+        for (int i = 0; i < arbitro.length; i++){
+        if (arbitro[i] != null && arbitro[i].equals(nombre)) {
+            arbitro[i] = null;
+            System.out.println("Arbitro eliminado");
+        }
+            }
+           }
+    
+     public void buscarArbitro(String nombre){
+        for (int i = 0; i < arbitro.length; i++) {
+        if (arbitro[i] != null && arbitro[i].equals(nombre)) {
+            System.out.println("Árbitro encontrado en la posición: " + i);
+            return;
+             }
+        }
+        System.out.println("Árbitro no encontrado");
+        }
+ 
+      public int contarArbitro(){
+        int contador = 0;
+        
+        for (int i = 0; i < arbitro.length; i++) {
+        if (arbitro[i] != null) {
+            contador++;
+        }
+    }
+        return contador;
+    }
+}
     
     
+
     
     
     
